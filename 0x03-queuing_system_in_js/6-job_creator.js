eed7fdd0c8ab812829b24d582bee6e9b79450ce8
gachafrejus <frejuskinf@gmail.com>
2024-12-05 02:39:07 -0800

    all tasks

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,23 @@
+import kue from 'kue';
+
+const queue = kue.createQueue({ name: 'push_notification_code' });
+
+const jobdata = {
+  phoneNumber: '+237693100322',
+  message: 'Test message',
+};
+
+const job = queue.create('push_notification_code', jobdata);
+
+job
+  .on('enqueue', () => {
+    console.log(`Notification job created: ${job.id}`);
+  })
+  .on('complete', () => {
+    console.log('Notification job completed');
+  })
+  .on('failed attempt', (err) => {
+    console.log(`Notification job failed`);
+  });
+
+job.save();
